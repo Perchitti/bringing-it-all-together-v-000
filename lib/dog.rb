1e732dceb7a68ab2f8d8195d51e8e2dfb783ad71
Lauren Perchitti <laurenperchitti@gmail.com>
2018-05-05 20:47:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,8 +5,6 @@ class Dog
 
     attr_accessor :id, :name, :breed
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
 
   def initialize(id=nil, name, breed)
       @id = id
