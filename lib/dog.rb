35c7162701b4854bdade32ec011cc10095b0cf5d
Lauren Perchitti <laurenperchitti@gmail.com>
2018-05-05 20:45:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -36,10 +36,10 @@ class Dog
           DB[:conn].execute(sql, self.name, self.breed, self.id)
       else
           sql = <<-SQL
-            INSERT INTO students (name, grade)
+            INSERT INTO dogs (name, breed)
             VALUES (?, ?);
           SQL
-          DB[:conn].execute(sql, self.name, self.grade)
+          DB[:conn].execute(sql, self.name, self.breed)
           @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
      end
   end
